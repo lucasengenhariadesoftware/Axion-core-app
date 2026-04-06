4e0cff9ba5e8fa2896275576ce6fb3132f79405b
Lucas e Silva <lucase.silvasoares1@gmail.com>
2026-04-06 04:00:30 -0300

    backup antes de mexer no antigravity

--- android/app/src/main/java/com/axioncore/app/MainActivity.java
+++ android/app/src/main/java/com/axioncore/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.axioncore.app;
 
+import android.os.Bundle;
+import android.graphics.Color;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setNavigationBarColor(Color.BLACK);
+    }
+}
